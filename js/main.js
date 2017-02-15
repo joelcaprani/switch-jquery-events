51de72567d5ce9612ce3f53aa4fadd36a9acc447
Joel Caprani <joelcaprani@Joels-MacBook-Air.local>
2017-02-15 18:14:13 -0500

    add text color change when switch changes

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ $(function() {
 
   var body = $('body');
   var button = $('button');
+  var status = $('status');
 
   $(".switch" ).on('click', function() {
     if(button.hasClass('on')) {
@@ -9,12 +10,15 @@ $(function() {
         body.removeClass('light');
         button.removeClass('on');
         button.addClass('off');
+        $('.status').text("Hey, who turned off the lights?");
+
 
         } else {
           body.addClass('light');
           body.removeClass('dark');
           button.removeClass('off');
           button.addClass('on');
+          $('.status').text("It's so bright in here!");
         }
 
   });
