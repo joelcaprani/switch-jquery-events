2c0b360712f31ecab23d454ddac61884badb3dbb
Joel Caprani <joelcaprani@Joels-MacBook-Air.local>
2017-02-15 17:14:16 -0500

    add switch and change background color

--- js/main.js
+++ js/main.js
@@ -1,2 +1,25 @@
+$(function() {
+
+  var body = $('body');
+  var button = $('button');
+
+  $(".switch" ).on('click', function() {
+    if(button.hasClass('on')) {
+        body.addClass('dark');
+        body.removeClass('light');
+        button.removeClass('on');
+        button.addClass('off');
+
+        } else {
+          body.addClass('light');
+          body.removeClass('dark');
+          button.removeClass('off');
+          button.addClass('on');
+        }
+
+  });
 
 
+
+
+});
